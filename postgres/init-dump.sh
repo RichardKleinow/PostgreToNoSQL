1024641db7ac4aa0fa6449a897bfd295ea0a1583
Richard Riss <richard.riss@desy.de>
2022-10-30 17:41:50 +0100

    every tar in folder postgres will be added as new database

--- postgres/init-dump.sh
+++ postgres/init-dump.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+for filename in /data/dump/*.tar; do
+	dbname=$(basename "$filename" .tar)
+	echo "Database dump ${dbname} found"
+	
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+		CREATE DATABASE "${dbname}";
+		GRANT ALL PRIVILEGES ON DATABASE "${dbname}" TO $POSTGRES_USER;
+	EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE DATABASE dvdrental;
-	GRANT ALL PRIVILEGES ON DATABASE dvdrental TO $POSTGRES_USER;
-EOSQL
-
-pg_restore -U "$POSTGRES_USER" -d dvdrental  /data/dump/dvdrental.tar
+	pg_restore -U "$POSTGRES_USER" -d "${dbname}"  "$filename"
+	echo "$filename restored to ${dbname}" 
+done
